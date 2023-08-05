861a9895ba7c49fcf75cd26c8f01e0578c4feed2
David Michael II <knowdavidmichael@gmail.com>
2023-08-05 23:13:45 +0100

    add MColors

---
+++ lib/core/theme/m_colors.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class MColors {
+  // Primary
+  static const Color primary50 = Color(0xFFEBE6F4);
+  static const Color primary75 = Color(0xFFAD96D1);
+  static const Color primary100 = Color(0xFF8A6BBF);
+  static const Color primary200 = Color(0xFF582BA3);
+  static const Color primary300 = Color(0xFF360090);
+  static const Color primary400 = Color(0xFF260065);
+  static const Color primary500 = Color(0xFF210058);
+  static const Color primary600 = Color(0xFF130033);
+  static const Color primary700 = Color(0xFF0C001F);
+
+  // Primary - Alt
+  static const Color primaryAlt = Color(0xFF2B213C);
+
+  // Secondary
+  static const Color secondary50 = Color(0xFFFFEAE6);
+  static const Color secondary75 = Color(0xFFFFA896);
+  static const Color secondary100 = Color(0xFFFF836B);
+  static const Color secondary200 = Color(0xFFFF4E2B);
+  static const Color secondary300 = Color(0xFFFF2A00);
+  static const Color secondary400 = Color(0xFFB31D00);
+  static const Color secondary500 = Color(0xFF9C1A00);
+  static const Color secondary600 = Color(0xFF661100);
+
+  // Success
+  static const Color success50 = Color(0xFFEBF6EE);
+  static const Color success75 = Color(0xFFADDBB8);
+  static const Color success100 = Color(0xFF8CCD9A);
+  static const Color success200 = Color(0xFF5AB76F);
+  static const Color success300 = Color(0xFF38A851);
+  static const Color success400 = Color(0xFF277639);
+  static const Color success500 = Color(0xFF226631);
+  static const Color success600 = Color(0xFF13391C);
+
+  // Yellow
+  static const Color yellow50 = Color(0xFFFCF6E8);
+  static const Color yellow75 = Color(0xFFF8E2B9);
+  static const Color yellow100 = Color(0xFFF6D69C);
+  static const Color yellow200 = Color(0xFFF2C471);
+  static const Color yellow300 = Color(0xFFEFB854);
+  static const Color yellow400 = Color(0xFFA7813B);
+  static const Color yellow500 = Color(0xFF927033);
+  static const Color yellow600 = Color(0xFF4C3A1A);
+
+  // Blue
+  static const Color blue50 = Color(0xFFEAF2FD);
+  static const Color blue75 = Color(0xFFA8C8F7);
+  static const Color blue100 = Color(0xFF85B1F4);
+  static const Color blue200 = Color(0xFF5090EF);
+  static const Color blue300 = Color(0xFF2C79EC);
+  static const Color blue400 = Color(0xFF1F55AF);
+  static const Color blue500 = Color(0xFF1B4A90);
+  static const Color blue600 = Color(0xFF102C56);
+  static const Color blue700 = Color(0xFF0A1A34);
+
+  // Error
+  static const Color error50 = Color(0xFFFDEAE7);
+  static const Color error75 = Color(0xFFF6A89B);
+  static const Color error100 = Color(0xFFF28472);
+  static const Color error200 = Color(0xFFED4F35);
+  static const Color error300 = Color(0xFFE92B0C);
+  static const Color error400 = Color(0xFFA31E08);
+  static const Color error500 = Color(0xFF8E1A07);
+  static const Color error600 = Color(0xFF5C1105);
+
+  // Grey
+  static const Color grey10 = Color(0xFFFAFBFB);
+  static const Color grey20 = Color(0xFFF5F6F7);
+  static const Color grey30 = Color(0xFFEBEDF0);
+  static const Color grey40 = Color(0xFFDFE2E6);
+  static const Color grey50 = Color(0xFFC2C7D0);
+  static const Color grey60 = Color(0xFFB3B9C4);
+  static const Color grey70 = Color(0xFFA6AEBB);
+  static const Color grey80 = Color(0xFF98A1B0);
+  static const Color grey90 = Color(0xFF8993A4);
+  static const Color grey100 = Color(0xFF7A8699);
+  static const Color grey200 = Color(0xFF6B788E);
+  static const Color grey300 = Color(0xFF5D6B82);
+  static const Color grey400 = Color(0xFF505F79);
+  static const Color grey500 = Color(0xFF42526D);
+  static const Color grey600 = Color(0xFF354764);
+  static const Color grey700 = Color(0xFF243757);
+  static const Color grey800 = Color(0xFF15294B);
+  static const Color grey900 = Color(0xFF091e42);
+
+  // Decorative/Yellow
+  static const Color decorativeYellow50 = Color(0xFFFFFFDF);
+  static const Color decorativeYellow75 = Color(0xFFffff2c);
+  static const Color decorativeYellow100 = Color(0xFFffff01);
+  static const Color decorativeYellow200 = Color(0xFFFFDC4D);
+
+  MColors._();
+}
