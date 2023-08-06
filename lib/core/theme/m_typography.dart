656b78ef7bb7113ae02f2043f3ba69ce25caee70
David Michael II <knowdavidmichael@gmail.com>
2023-08-06 02:21:05 +0100

    add MTypography class

---
+++ lib/core/theme/m_typography.dart
@@ -0,0 +1,166 @@
+import 'dart:ui';
+
+class MTypography {
+  MTypography._();
+
+  static const String _fontFamily = "SFProDisplay";
+
+  // Heading 1
+  static final TextStyle heading1Bold = TextStyle(
+    fontSize: 32.0,
+    height: 40.0 / 32.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle heading1Medium = TextStyle(
+    fontSize: 32.0,
+    height: 40.0 / 32.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle heading1Regular = TextStyle(
+    fontSize: 32.0,
+    height: 40.0 / 32.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+  );
+
+  // Heading 2
+  static final TextStyle heading2Bold = TextStyle(
+    fontSize: 24.0,
+    height: 32.0 / 24.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle heading2Medium = TextStyle(
+    fontSize: 24.0,
+    height: 32.0 / 24.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle heading2Regular = TextStyle(
+    fontSize: 24.0,
+    height: 32.0 / 24.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+  );
+
+  // Subheading
+  static final TextStyle subheadingBold = TextStyle(
+    fontSize: 16.0,
+    height: 24.0 / 16.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle subheadingMedium = TextStyle(
+    fontSize: 16.0,
+    height: 24.0 / 16.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle subheadingRegular = TextStyle(
+    fontSize: 16.0,
+    height: 24.0 / 16.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+  );
+
+  // Body
+  static final TextStyle bodyBold = TextStyle(
+    fontSize: 16.0,
+    height: 24.0 / 16.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle bodyMedium = TextStyle(
+    fontSize: 16.0,
+    height: 24.0 / 16.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle bodyRegular = TextStyle(
+    fontSize: 16.0,
+    height: 24.0 / 16.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+  );
+
+  // Caption
+  static final TextStyle captionBold = TextStyle(
+    fontSize: 14.0,
+    height: 16.0 / 14.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+  );
+  static final TextStyle captionMedium = TextStyle(
+    fontSize: 14.0,
+    height: 16.0 / 14.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle captionRegular = TextStyle(
+    fontSize: 14.0,
+    height: 16.0 / 14.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+  );
+
+  // Micro
+  static final TextStyle microBold = TextStyle(
+    fontSize: 12.0,
+    height: 16.0 / 12.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle microMedium = TextStyle(
+    fontSize: 12.0,
+    height: 16.0 / 12.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle microRegular = TextStyle(
+    fontSize: 12.0,
+    height: 16.0 / 12.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+  );
+
+  // Nano
+  static final TextStyle nanoBold = TextStyle(
+    fontSize: 10.0,
+    height: 14.0 / 10.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+  );
+  static final TextStyle nanoMedium = TextStyle(
+    fontSize: 10.0,
+    height: 14.0 / 10.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle nanoRegular = TextStyle(
+    fontSize: 10.0,
+    height: 14.0 / 10.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
+  );
+
+  static final TextStyle buttonMedium = TextStyle(
+    fontSize: 8.0,
+    height: 16.0 / 8.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+}
