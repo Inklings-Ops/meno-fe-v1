f715272f9c5a7427697bb3e3b901820f793f715e
David Michael II <knowdavidmichael@gmail.com>
2023-08-06 02:41:18 +0100

    add typography

--- lib/core/theme/m_typography.dart
+++ lib/core/theme/m_typography.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class MTypography {
   MTypography._();
@@ -6,161 +6,183 @@ class MTypography {
   static const String _fontFamily = "SFProDisplay";
 
   // Heading 1
-  static final TextStyle heading1Bold = TextStyle(
+  static const TextStyle heading1Bold = TextStyle(
     fontSize: 32.0,
     height: 40.0 / 32.0,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
+    debugLabel: "Heading 1 Bold",
   );
 
-  static final TextStyle heading1Medium = TextStyle(
+  static const TextStyle heading1Medium = TextStyle(
     fontSize: 32.0,
     height: 40.0 / 32.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Heading 1 Medium",
   );
 
-  static final TextStyle heading1Regular = TextStyle(
+  static const TextStyle heading1Regular = TextStyle(
     fontSize: 32.0,
     height: 40.0 / 32.0,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily,
+    debugLabel: "Heading 1 Regular",
   );
 
   // Heading 2
-  static final TextStyle heading2Bold = TextStyle(
+  static const TextStyle heading2Bold = TextStyle(
     fontSize: 24.0,
     height: 32.0 / 24.0,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
+    debugLabel: "Heading 2 Bold",
   );
 
-  static final TextStyle heading2Medium = TextStyle(
+  static const TextStyle heading2Medium = TextStyle(
     fontSize: 24.0,
     height: 32.0 / 24.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Heading 2 Medium",
   );
 
-  static final TextStyle heading2Regular = TextStyle(
+  static const TextStyle heading2Regular = TextStyle(
     fontSize: 24.0,
     height: 32.0 / 24.0,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily,
+    debugLabel: "Heading 2 Regular",
   );
 
   // Subheading
-  static final TextStyle subheadingBold = TextStyle(
+  static const TextStyle subheadingBold = TextStyle(
     fontSize: 16.0,
     height: 24.0 / 16.0,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
+    debugLabel: "Subheading Bold",
   );
 
-  static final TextStyle subheadingMedium = TextStyle(
+  static const TextStyle subheadingMedium = TextStyle(
     fontSize: 16.0,
     height: 24.0 / 16.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Subheading Medium",
   );
 
-  static final TextStyle subheadingRegular = TextStyle(
+  static const TextStyle subheadingRegular = TextStyle(
     fontSize: 16.0,
     height: 24.0 / 16.0,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily,
+    debugLabel: "Subheading Regular",
   );
 
   // Body
-  static final TextStyle bodyBold = TextStyle(
+  static const TextStyle bodyBold = TextStyle(
     fontSize: 16.0,
     height: 24.0 / 16.0,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
+    debugLabel: "Body Bold",
   );
 
-  static final TextStyle bodyMedium = TextStyle(
+  static const TextStyle bodyMedium = TextStyle(
     fontSize: 16.0,
     height: 24.0 / 16.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Body Medium",
   );
 
-  static final TextStyle bodyRegular = TextStyle(
+  static const TextStyle bodyRegular = TextStyle(
     fontSize: 16.0,
     height: 24.0 / 16.0,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily,
+    debugLabel: "Body Regular",
   );
 
   // Caption
-  static final TextStyle captionBold = TextStyle(
+  static const TextStyle captionBold = TextStyle(
     fontSize: 14.0,
     height: 16.0 / 14.0,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
+    debugLabel: "Caption Bold",
   );
-  static final TextStyle captionMedium = TextStyle(
+  static const TextStyle captionMedium = TextStyle(
     fontSize: 14.0,
     height: 16.0 / 14.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Caption Medium",
   );
 
-  static final TextStyle captionRegular = TextStyle(
+  static const TextStyle captionRegular = TextStyle(
     fontSize: 14.0,
     height: 16.0 / 14.0,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily,
+    debugLabel: "Caption Regular",
   );
 
   // Micro
-  static final TextStyle microBold = TextStyle(
+  static const TextStyle microBold = TextStyle(
     fontSize: 12.0,
     height: 16.0 / 12.0,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
+    debugLabel: "Micro Bold",
   );
 
-  static final TextStyle microMedium = TextStyle(
+  static const TextStyle microMedium = TextStyle(
     fontSize: 12.0,
     height: 16.0 / 12.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Micro Medium",
   );
 
-  static final TextStyle microRegular = TextStyle(
+  static const TextStyle microRegular = TextStyle(
     fontSize: 12.0,
     height: 16.0 / 12.0,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily,
+    debugLabel: "Micro Regular",
   );
 
   // Nano
-  static final TextStyle nanoBold = TextStyle(
+  static const TextStyle nanoBold = TextStyle(
     fontSize: 10.0,
     height: 14.0 / 10.0,
     fontWeight: FontWeight.w700,
     fontFamily: _fontFamily,
+    debugLabel: "Nano Bold",
   );
-  static final TextStyle nanoMedium = TextStyle(
+  static const TextStyle nanoMedium = TextStyle(
     fontSize: 10.0,
     height: 14.0 / 10.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Nano Medium",
   );
 
-  static final TextStyle nanoRegular = TextStyle(
+  static const TextStyle nanoRegular = TextStyle(
     fontSize: 10.0,
     height: 14.0 / 10.0,
     fontWeight: FontWeight.w400,
     fontFamily: _fontFamily,
+    debugLabel: "Nano Regular",
   );
 
-  static final TextStyle buttonMedium = TextStyle(
+  static const TextStyle buttonMedium = TextStyle(
     fontSize: 8.0,
     height: 16.0 / 8.0,
     fontWeight: FontWeight.w500,
     fontFamily: _fontFamily,
+    debugLabel: "Button Medium",
   );
 }
