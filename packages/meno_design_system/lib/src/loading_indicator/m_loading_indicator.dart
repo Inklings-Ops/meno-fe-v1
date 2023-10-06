fe9fc0127dd6f56d5f043e506e6bd0f1c46ad7c9
David Michael II <knowdavidmichael@gmail.com>
2023-10-06 21:08:22 +0100

    add box factory to MLoadingIndicator

--- packages/meno_design_system/lib/src/loading_indicator/m_loading_indicator.dart
+++ packages/meno_design_system/lib/src/loading_indicator/m_loading_indicator.dart
@@ -2,44 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:meno_design_system/meno_design_system.dart';
 
 class MLoadingIndicator extends StatelessWidget {
-  final double height;
-  final double width;
-
-  /// Creates a 'default' sized loading indicator.
-  const MLoadingIndicator({Key? key}) : this.five(key: key);
+  final double? height;
+  final double? width;
 
   /// Creates a custom loading indicator.
-  const MLoadingIndicator.custom(this.height, this.width, {super.key});
+  const MLoadingIndicator._(this.height, this.width, {super.key});
+
+  const MLoadingIndicator(this.height, this.width, {super.key});
 
   const MLoadingIndicator.five({
     Key? key,
     double height = 48,
     double width = 48,
-  }) : this.custom(height, width, key: key);
+  }) : this._(height, width, key: key);
 
   const MLoadingIndicator.four({
     Key? key,
     double height = 40,
     double width = 40,
-  }) : this.custom(height, width, key: key);
+  }) : this._(height, width, key: key);
 
   const MLoadingIndicator.one({
     Key? key,
     double height = 1200,
     double width = 1200,
-  }) : this.custom(height, width, key: key);
+  }) : this._(height, width, key: key);
 
   const MLoadingIndicator.three({
     Key? key,
     double height = 32,
     double width = 32,
-  }) : this.custom(height, width, key: key);
+  }) : this._(height, width, key: key);
 
   const MLoadingIndicator.two({
     Key? key,
     double height = 24,
     double width = 24,
-  }) : this.custom(height, width, key: key);
+  }) : this._(height, width, key: key);
+
+  const factory MLoadingIndicator.box({
+    Key? key,
+    double? height,
+    double? width,
+  }) = _BoxLoadingIndicator;
 
   @override
   Widget build(BuildContext context) {
@@ -47,6 +52,30 @@ class MLoadingIndicator extends StatelessWidget {
       height: height,
       width: width,
       child: Assets.images.loading.image(fit: BoxFit.cover),
+    );
+  }
+}
+
+class _BoxLoadingIndicator extends MLoadingIndicator {
+  const _BoxLoadingIndicator({
+    Key? key,
+    double? height,
+    double? width,
+  }) : super._(height, width, key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final MColorScheme colorScheme = MColorScheme.of(context)!;
+    return Center(
+      child: Container(
+        height: height ?? MediaQuery.sizeOf(context).width * 0.2,
+        width: width ?? MediaQuery.sizeOf(context).width * 0.2,
+        decoration: BoxDecoration(
+          color: colorScheme.background,
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Assets.images.loading.image(),
+      ),
     );
   }
 }
