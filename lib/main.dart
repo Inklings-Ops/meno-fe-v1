2460cca4256b3480afd39e835ae8fe3496721a91
David Michael II <knowdavidmichael@gmail.com>
2023-11-23 22:02:10 +0100

    config: add firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 import 'src/dependency_injector/injector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await configureDependencies();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // await di<SharedPreferences>().clear();
   // await di<SecureStorageService>().deleteAll();
   runApp(const ProviderScope(child: MenoApp()));
