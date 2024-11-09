9a4a8edf2983a668bf337e3f7228486ff7e1579b
David Michael II <knowdavidmichael@gmail.com>
2024-11-09 23:11:06 +0100

    add slight implementation of background processes

--- lib/src/shared/m_keys.dart
+++ lib/src/shared/m_keys.dart
@@ -1,5 +1,4 @@
 class MKeys {
-
   MKeys._();
   static const String authUserCredentialKey = '_user_credentials_';
   static const String allUserCredentialsKey = '_all_user_credentials_';
@@ -7,4 +6,9 @@ class MKeys {
   static const String onboardingKey = '_onboarding_';
   static const String fcmToken = '_fcm_Token';
   static const String settings = '_settings_';
+  static const String notificationChannelId = '_meno_notification_channel_';
+  static const int notificationId = 888;
+  static const String broadcastBackgroundTask = '_broadcast_background_task_';
+  static const String streamBackgroundTask = '_stream_background_task_';
+  static const String endBackgroundTask = '_end_background_task_';
 }
