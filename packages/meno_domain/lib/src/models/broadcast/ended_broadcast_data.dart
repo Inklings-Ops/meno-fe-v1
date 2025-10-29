6e657381064332220eae50aa14dc00983cb3e281
David Michael II <knowdavidmichael@gmail.com>
2025-10-30 00:26:52 +0100

    feat: add ended broadcast data model

---
+++ packages/meno_domain/lib/src/models/broadcast/ended_broadcast_data.dart
@@ -0,0 +1,40 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:meno_domain/meno_domain.dart';
+
+part 'ended_broadcast_data.freezed.dart';
+
+part 'ended_broadcast_data.g.dart';
+
+/// Represents the data associated with an ended broadcast.
+@freezed
+abstract final class EndedBroadcastData with _$EndedBroadcastData {
+  /// Creates an [EndedBroadcastData] object.
+  const factory EndedBroadcastData({
+    /// The details of the broadcast that has ended.
+    required Broadcast broadcastDetails,
+
+    /// The reason why the broadcast ended.
+    required EndedBroadcastReason reason,
+  }) = _EndedBroadcastData;
+
+  /// Creates an [EndedBroadcastData] from a JSON object.
+  factory EndedBroadcastData.fromJson(Map<String, dynamic> json) =>
+      _$EndedBroadcastDataFromJson(json);
+}
+
+/// Represents the reason why a broadcast ended.
+@freezed
+abstract final class EndedBroadcastReason with _$EndedBroadcastReason {
+  /// Creates an [EndedBroadcastReason] object.
+  const factory EndedBroadcastReason({
+    /// The type of reason for the broadcast ending.
+    required String type,
+
+    /// The message associated with the reason for the broadcast ending.
+    required String message,
+  }) = _EndedBroadcastReason;
+
+  /// Creates an [EndedBroadcastReason] from a JSON object.
+  factory EndedBroadcastReason.fromJson(Map<String, dynamic> json) =>
+      _$EndedBroadcastReasonFromJson(json);
+}
