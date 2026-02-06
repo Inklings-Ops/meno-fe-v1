ab7794ddc45fbb9b92afbb8b241975e2ed2dcba0
David Michael II <knowdavidmichael@gmail.com>
2026-02-06 03:27:49 +0100

    add `flutter_secure_storage` dependency and register in DI
    This commit adds the `flutter_secure_storage` package to the project to enable secure data persistence across multiple platforms.

--- lib/di.dart
+++ lib/di.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_it/flutter_it.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:meno/app/router/router.dart';
 
 void injectDependencies() {
+  di.registerLazySingleton(FlutterSecureStorage.new);
   di.registerSingleton(MRouter.new);
 }
