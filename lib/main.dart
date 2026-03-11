3eb25e2ff1bba43ddca2451642c874c46dfb48ed
David Michael II <knowdavidmichael@gmail.com>
2026-03-11 18:39:16 +0100

    feat(core): enable global dependencies and exception handling in app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:meno/_core/exceptions/global_exception_handler.dart';
+import 'package:meno/_di/global_locator.dart';
 import 'package:meno/app.dart';
 
-Future<void> main() async {
+void main() {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  // await configureGlobalDependencies();
-  // configureGlobalExceptionHandler();
+  configureGlobalDependencies();
+  configureGlobalExceptionHandler();
 
   runApp(const MenoApp());
 }
