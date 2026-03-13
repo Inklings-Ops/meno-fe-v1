a3fa5d170db50b82839d3c6b70be31034bf3062d
David Michael II <knowdavidmichael@gmail.com>
2026-03-13 02:32:31 +0100

    chore: update package names and bundle identifiers for consistency across platforms
    - Adjusted `namespace` and `applicationId` in Android build configuration.
    - Updated `PRODUCT_BUNDLE_IDENTIFIER` in macOS and iOS project files.
    - Applied consistent package naming by removing redundant `.meno` suffix.
    - Upgraded dependencies in `pubspec.lock` to latest minor versions.
    - Fixed duplicate viewport meta tag in `web/index.html`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.inklings.meno.meno"
+    namespace = "com.inklings.meno"
     compileSdk = (rootProject.property("compileSdk") as String).toInt()
     ndkVersion = rootProject.property("ndkVersion") as String
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.inklings.meno.meno"
+        applicationId = "com.inklings.meno"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
